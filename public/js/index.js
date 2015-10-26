3813cfd43b782f2601a8e0c41931a5b5507306ed
Letty Fuentes <letty@Lettys-Mac-mini.local>
2015-10-25 21:40:19 -0500

    Simple Simon Mickey Mouse

--- public/js/index.js
+++ public/js/index.js
@@ -1,105 +1,110 @@
-(function (){
+$(document).ready(function(){
 "use stritct";
 
-// var randomColor = "";
 var simonSequence = [];
-var userSequence = [];
+var playerSequence = [];
 
-
-var simonColorGenerator = function (){
+function simonGeneratesSequence(){
 	var maxNumber = 4;
 	var simonButtons = $('.simon-button'); //store the entire DIV in simonButtons -jQuery
 	var randomNumber;
 	
-
 	var randomNumber = Math.floor(Math.random() * maxNumber);
-	console.log(randomNumber);
-	// console.log(simonButtons[randomNumber]);
 
 	var randomColor = simonButtons[randomNumber];
-	$(randomColor).animate({
-				opacity: 0.50,
-			}, 250, function () {
-				// console.log ($(randomColor)); 
-				// console.log ($(randomColor($("#simon-red").html() )));
 
+	$("#player-messages").fadeIn(3000, function() {
+    	$("#player-messages").html("Simon's Turn")
+  	});
+
+	$(randomColor).animate({
+				opacity: 0.50
+			}, 750).animate({
+				opacity: 1.0
+			}, 750, function () {
 				//push the color into the simonSequence array
-				// simonSequence.push (randomColor.getAttribute('id')); //pushes to the end of the array
 				simonSequence.push(randomNumber);
-	});
-	//not sure if I need the return value anymore
-	return simonButtons[randomNumber]; //Returns DIV 0..3 depending on the random #-JavaScript
+				console.log ("simonSequence array: " + simonSequence);
+			});
+	// return simonButtons[randomNumber]; //Returns DIV 0..3 depending on the random #-JavaScript
 }
 
-var userMatchesSequence = function (){
-	//allow the user to enter values within 10 seconds otherwise they are doomed
+function playerMatchesSequence(){
+	//allow the player to enter values within 10 seconds otherwise they are doomed
+	var buttonClicked;
 
-	//store the value
-	$("id").click(
-		function() {
+	$("#player-messages").fadeIn(5000, function() {
+    	$("#player-messages").html("Player's Turn")
+  	});
+
+	$(".simon-button").click( 
+		function(event) {
+			//This pushes the item twice into the array???
+			buttonClicked = $(this).attr("data-button-number");
+
 			$(this).animate({
 				opacity: 0.50,
-			}, 250).animate ({
+			}, 500).animate ({
 				opacity: 1.0,
-			}, 250);
+			}, 500, function () {
+				playerSequence.push(buttonClicked);
+				console.log ("playerSequence array: " + playerSequence);
+			});
+			event.stopPropagation();
 
-			userSequence.push();
 		}
 	);		
-
-	//make the comparison with simonArray
-	//perform necessary actions depending on true or false - for false alert the user
-	//return true or false - don't have a return statement inside an annonymous function
 }
 
+function matchingSequences(){
+	console.log("matching sequences function");
+	//make the comparison with simonArray
+	if (playerSequence[playerSequence.length-1] === simonSequence[simonSequence.length-1]){
+		// var confirmed = confirm ("Are you ready for the next round?");
+		console.log("player pressed correct color");
+		return true;
+	} else {
+		console.log ("You lost, press start to try again!");
+		return false;
+	};
+}
 
+function updateRoundCounter (currentRound) {
+	$('#dynamic-round-number').html(currentRound++);
+	return currentRound++;
+}
 
-
-$("start-button").click(
-function() {
-	//can use $(this).function() when needed
-	var userIsAwesome = true;
+$(".start-button").click(
+function(event) {
+	var playerIsAwesome = true;
 	var round = 1;
-
 	// do {
-		//computer turn
-		randomColor = simonColorGenerator(); //not sure I need a return value right here
+		//simon turn
+		simonGeneratesSequence();
 
-		//users turn
-		userIsAwesome = userMatchesSequence (); //returns true if matching sequence; false otherwise
-
-		//increase the round if successful
-		round++;
-
-		
+		//player turn
+	    playerMatchesSequence();
+    	event.stopPropagation();
 
 
-		// users turn
-	// } while (userIsAwesome  == true);		
+	    //verify that the sequences match
+	    playerIsAwesome = matchingSequences();
+	    if (playerIsAwesome === true){
+	    	round = updateRoundCounter(round);
+	    }
+	    else {
+	    	simonSequence = [];
+			playerSequence = [];
+			round = 1;
+			playerIsAwesome = true;
+	    }
 
+		//play the sequences again with another round
+
+	// } while (playerIsAwesome  == true);		
 });
 
 
 
 
-
-
-//Event Handler - fade the color in and out if mouse enters the shape
-// $(".simon-button").hover(
-// 	function() {
-// 		$(this).animate({
-// 			opacity: 0.50,
-// 		}, 250, function () {
-// 			//Do nothing
-// 		});
-
-// 	}, function(){
-// 		$(this).animate({
-// 			opacity: 1.0,
-// 		}, 250, function () {
-// 			//Do nothing
-// 		});
-// 	}
-// );
-
-})();
+});
